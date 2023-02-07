c3824de5e550d37466d8bd09c233107ab46976f1
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2023-02-07 13:23:57 +0530

    vercel api

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const appApi = createApi({
   reducerPath: 'appApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://realtimechatappbackend-production.up.railway.app',
+    baseUrl: 'https://realtime-chat-app-backend.vercel.app',
   }),
 
   endpoints: (builder) => ({
